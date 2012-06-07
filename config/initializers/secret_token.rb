c2a252d60dc0e37fb80ae851480b7a0cd563881c
Chris Yeoh <christoperyeoh@gmail.com>
2012-06-07 16:13:38 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberExample::Application.config.secret_token = '7045d88ba98dda904f11486b8d3e8f6fbd0b4262371275883d8a73d3112e10dd2908c4a8653d57c8cbdda2ff00a40d86072b1217ea31726cf4c8907a698034ce'
